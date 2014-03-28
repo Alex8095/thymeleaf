2372bbca9e8a75d5324f431035d51d72d94e7e9c
Alex <alex.8095@gmail.com>
2014-03-28 11:45:11 +0300

    first commit

---
+++ src/main/java/com/frogorf/thymeleaf/web/controller/HomeController.java
@@ -0,0 +1,52 @@
+package com.frogorf.thymeleaf.web.controller;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@RequestMapping("/home")
+@Controller
+public class HomeController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	private static List<String> data = new ArrayList<String>();
+	
+	private static Set<String> dataSet = new HashSet<String>();
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		String formattedDate = dateFormat.format(date);
+		model.addAttribute("serverTime", formattedDate);
+		
+		dataSet.add("a");
+		dataSet.add("b");
+		dataSet.add("c");
+		dataSet.add("d");
+		
+		model.addAttribute("dataSet", dataSet);
+				
+		return "home";
+	}
+
+}
